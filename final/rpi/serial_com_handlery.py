f316573bf8915103b3db90eceb5b33c68f421072
gohara23 <59593124+gohara23@users.noreply.github.com>
2022-03-21 14:49:04 -0400

    com handler with json receive

--- final/rpi/serial_com_handlery.py
+++ final/rpi/serial_com_handlery.py
@@ -1,14 +1,37 @@
 import serial
+import json
+import time
 from globals import *
 
 
 class ComHandler:
     def __init__(self, com_port, baud_rate=9600):
-        ser = serial.Serial(com_port, baud_rate, timeout=1)
-        ser.flush()
+        self.ser = serial.Serial(com_port, baud_rate, timeout=1)
+        self.ser.flush()
+
+    def send_command(self, command):
+        self.ser.write(f"{command}\n")
+
+    def get_data(self, command):
+        self.ser.write(str.encode(f"{command}\n"))
+        while not self.ser.in_waiting:
+            time.sleep(0.01)
+        line = self.ser.readline().decode("utf-8").rstrip()
+        return line
+        
+
+
 
 
 if __name__ == "__main__":
 
     COM_PORT = "COM13"
     handler = ComHandler(COM_PORT)
+
+    cmd = "baud"
+    while True:
+        line = handler.get_data(f"{cmd}\n")
+        print(line)
+        dictionary = json.loads(line)
+        print(dictionary["baud_rate"])
+        time.sleep(5)
