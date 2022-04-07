0296c4217c8f82ce70fc8f7cb0614bdbe5a632d7
gohara23 <59593124+gohara23@users.noreply.github.com>
2022-04-07 10:54:45 -0400

    2 cameras

--- examples/pi_camera/usb_cam.py
+++ examples/pi_camera/usb_cam.py
@@ -1,12 +1,16 @@
 import numpy as np
 import cv2 as cv
-cap = cv.VideoCapture(1)
+cap = cv.VideoCapture(2)
+cap2 = cv.VideoCapture(0)
+
 if not cap.isOpened():
     print("Cannot open camera")
     exit()
 while True:
     # Capture frame-by-frame
     ret, frame = cap.read()
+    ret2, frame2 = cap2.read()
+
     # if frame is read correctly ret is True
     if not ret:
         print("Can't receive frame (stream end?). Exiting ...")
@@ -15,6 +19,8 @@ while True:
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     # Display the resulting frame
     cv.imshow('frame', frame)
+    cv.imshow('frame', frame2)
+    
     if cv.waitKey(1) == ord('q'):
         break
 # When everything done, release the capture
