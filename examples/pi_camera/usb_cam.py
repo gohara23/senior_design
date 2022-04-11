adb203de7597074051501e86fbd42afc5c0426c4
gohara23 <59593124+gohara23@users.noreply.github.com>
2022-04-11 02:09:48 -0400

    cam port test

--- examples/pi_camera/usb_cam.py
+++ examples/pi_camera/usb_cam.py
@@ -1,32 +1,67 @@
 import numpy as np
 import cv2 as cv
-cap = cv.VideoCapture(0)
-cap2 = cv.VideoCapture(1)
 
-if not cap.isOpened():
-    print("Cannot open camera")
-    exit()
-while True:
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-    ret2, frame2 = cap2.read()
 
-    # if frame is read correctly ret is True
-    if not ret:
-        print("Cam 1 err")
-        break
+def list_ports():
+    is_working = True
+    dev_port = 0
+    working_ports = []
+    available_ports = []
+    while is_working:
+        camera = cv.VideoCapture(dev_port)
+        if not camera.isOpened():
+            is_working = False
+            print("Port %s is not working." % dev_port)
+        else:
+            is_reading, img = camera.read()
+            w = camera.get(3)
+            h = camera.get(4)
+            if is_reading:
+                print("Port %s is working and reads images (%s x %s)" %
+                        (dev_port, h, w))
+                working_ports.append(dev_port)
+            else:
+                print("Port %s for camera ( %s x %s) is present but does not reads." % (
+                    dev_port, h, w))
+                available_ports.append(dev_port)
+        dev_port += 1
+    return available_ports, working_ports
 
-    if not ret2:
-        print("Cam 2 err")
-        break
-    # Our operations on the frame come here
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    # Display the resulting frame
-    cv.imshow('frame', frame)
-    cv.imshow('frame', frame2)
-    
-    if cv.waitKey(1) == ord('q'):
-        break
-# When everything done, release the capture
-cap.release()
-cv.destroyAllWindows()
+
+if __name__ == "__main__":
+
+    ports, working = list_ports()
+
+    print(ports)
+    print(working)
+
+# cap = cv.VideoCapture(0)
+# cap2 = cv.VideoCapture(1)
+
+# if not cap.isOpened():
+#     print("Cannot open camera")
+#     exit()
+# while True:
+#     # Capture frame-by-frame
+#     ret, frame = cap.read()
+#     ret2, frame2 = cap2.read()
+
+#     # if frame is read correctly ret is True
+#     if not ret:
+#         print("Cam 1 err")
+#         break
+
+#     if not ret2:
+#         print("Cam 2 err")
+#         break
+#     # Our operations on the frame come here
+#     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+#     # Display the resulting frame
+#     cv.imshow('frame', frame)
+#     cv.imshow('frame', frame2)
+
+#     if cv.waitKey(1) == ord('q'):
+#         break
+# # When everything done, release the capture
+# cap.release()
+# cv.destroyAllWindows()
