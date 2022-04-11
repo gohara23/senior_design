2dbe58accfb63c7ea5f23b41499e5f97c2d1a11d
gohara23 <59593124+gohara23@users.noreply.github.com>
2022-04-11 02:13:23 -0400

    cam test port 0

--- examples/pi_camera/usb_cam.py
+++ examples/pi_camera/usb_cam.py
@@ -35,33 +35,33 @@ if __name__ == "__main__":
     print(ports)
     print(working)
 
-# cap = cv.VideoCapture(0)
+    cap = cv.VideoCapture(0)
 # cap2 = cv.VideoCapture(1)
 
-# if not cap.isOpened():
-#     print("Cannot open camera")
-#     exit()
-# while True:
-#     # Capture frame-by-frame
-#     ret, frame = cap.read()
-#     ret2, frame2 = cap2.read()
+    if not cap.isOpened():
+        print("Cannot open camera")
+        exit()
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    # ret2, frame2 = cap2.read()
 
-#     # if frame is read correctly ret is True
-#     if not ret:
-#         print("Cam 1 err")
-#         break
+    # if frame is read correctly ret is True
+    if not ret:
+        print("Cam 1 err")
+        break
 
-#     if not ret2:
-#         print("Cam 2 err")
-#         break
-#     # Our operations on the frame come here
-#     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-#     # Display the resulting frame
-#     cv.imshow('frame', frame)
-#     cv.imshow('frame', frame2)
+    # if not ret2:
+    #     print("Cam 2 err")
+    #     break
+    # Our operations on the frame come here
+    # gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    # Display the resulting frame
+    cv.imshow('frame', frame)
+    # cv.imshow('frame', frame2)
 
-#     if cv.waitKey(1) == ord('q'):
-#         break
-# # When everything done, release the capture
-# cap.release()
-# cv.destroyAllWindows()
+    if cv.waitKey(1) == ord('q'):
+        break
+# When everything done, release the capture
+cap.release()
+cv.destroyAllWindows()
